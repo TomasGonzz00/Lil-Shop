ed9d8f31550ebf825df7c957b85fc21dcbd279a7
TomasGonzz00 <tomasgonzalez4278@gmail.com>
2022-12-11 20:49:20 -0300

    Incorporando itemCount con Evento

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,10 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 
-
-const ItemCount = ({stock, initial, onAdd}) => {
-    console.log(stock,initial)
+const ItemCount = ({ stock, count, onAdd }) => {
     return (
         <div>
-            <p className="prueba2">{stock} {initial}</p>
+            <div className='item-count'>             
+                <button onClick={() => onAdd("minus")} className="cart-button">
+                    <FontAwesomeIcon icon={faMinus} />
+                </button>
+                <p className="prueba2">{count}</p>
+                <button onClick={() => onAdd("plus")} className="cart-button">
+                    <FontAwesomeIcon icon={faPlus} />
+                </button>
+            </div>
         </div>
     );
 }
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,7 +1,22 @@
 import ItemCount from './ItemCount';
 import Carousel from 'react-bootstrap/Carousel';
+import { useState, useEffect } from 'react';
 
 const ItemDetail = ({ item }) => {
+    const [count, setCount] = useState(0);
+    const [stock, setStock] = useState('');
+    const max = stock;
+
+    useEffect(() => {
+            setStock(item.stock);
+      }, [item.stock]);
+
+    
+
+    function onAdd(algo) {
+        if (algo === "plus" && count < max) setCount(count + 1);
+        if (algo === "minus" && count > 0) setCount(count - 1);
+    }
     return (
         <div className='item-detail-container'>
             <Carousel className='card-detail-carousel'>
@@ -31,7 +46,12 @@ const ItemDetail = ({ item }) => {
                 <p className='card-title' >{item.name}</p>
                 <p className='card-description'>{item.description}</p>
                 <p className='card-precio' >${item.precio}</p>
-                <ItemCount stock="5" initial="1"  />
+                <p className='card-precio' >Stock: {item.stock}</p>
+                {max > 0 ? (
+            <ItemCount stock={stock} count={count} onAdd={onAdd} />
+          ) : (
+            <span className="card-precio">Sin stock</span>
+          )}
             </div>
         </div>
     )
